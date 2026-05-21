92dfb3487bba229c004bee721de69a42c56ea174
Arlo Belshee <arlo@belshee.com>
2026-05-21 00:46:26 -0400

    . f Add three safety matrix UI prototypes (ProtoA/B/C) with DDR brand palette

--- src/src/router.ts
+++ src/src/router.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import OverviewView from './views/OverviewView.vue'
 import FateChoiceDetailView from './views/FateChoiceDetailView.vue'
 import PrimaryAxisView from './views/PrimaryAxisView.vue'
+import ProtoA from './views/ProtoA.vue'
+import ProtoB from './views/ProtoB.vue'
+import ProtoC from './views/ProtoC.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -21,7 +24,10 @@ const router = createRouter({
       name: 'fate-choice-detail',
       component: FateChoiceDetailView,
       props: true
-    }
+    },
+    { path: '/proto-a', name: 'proto-a', component: ProtoA },
+    { path: '/proto-b', name: 'proto-b', component: ProtoB },
+    { path: '/proto-c', name: 'proto-c', component: ProtoC },
   ]
 })
 
